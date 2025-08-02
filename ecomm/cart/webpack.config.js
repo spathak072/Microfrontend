75d2856e8773faf6e1cf1cbe9a5d82fdb3671b59
spathak072 <saurabhpathak072@gmail.com>
2025-08-02 15:15:45 +0530

    added cart

---
+++ ecomm/cart/webpack.config.js
@@ -0,0 +1,42 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {ModuleFederationPlugin} = require("webpack").container;
+module.exports={
+mode:"development",
+ entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+    devServer: {
+    static: './dist',
+    port: 8082,
+    open: true
+  },
+plugins:[
+    new ModuleFederationPlugin({
+        name:'cart',
+        filename:'remoteEntry.js',
+        exposes:{
+            './CartShow':'./src/index'
+        }
+    }),
+    new HtmlWebpackPlugin({
+        template:'./public/index.html'
+    })
+]
+}
