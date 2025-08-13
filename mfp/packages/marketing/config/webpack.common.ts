91fa7a169e88efbe74c535ce0692863ae6b29789
spathak072 <saurabhpathak072@gmail.com>
2025-08-13 23:13:16 +0530

    configured Marketing app

---
+++ mfp/packages/marketing/config/webpack.common.ts
@@ -0,0 +1,27 @@
+module.exports={
+    module:{
+        rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'ts-loader',
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+}
