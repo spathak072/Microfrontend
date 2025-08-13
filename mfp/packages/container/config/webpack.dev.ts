24ef83aa50d766cb595117d3ce6e27ca0bb8626d
spathak072 <saurabhpathak072@gmail.com>
2025-08-14 00:26:48 +0530

    Configure container app

---
+++ mfp/packages/container/config/webpack.dev.ts
@@ -0,0 +1,21 @@
+const {merge} = require('webpack-merge');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig ={
+    mode: 'development',
+    devServer: {
+        hot: true,
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html',
+        },
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
