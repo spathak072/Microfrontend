83316638ba9c155fdb71f94877e2b538cb4e1dc6
spathak072 <saurabhpathak072@gmail.com>
2025-08-31 00:01:01 +0530

    Capture navigation from container

--- mfp/packages/container/src/Components/MarketingApp.tsx
+++ mfp/packages/container/src/Components/MarketingApp.tsx
@@ -5,7 +5,11 @@ import React, {useRef, useEffect} from 'react'
 const MarketingApp = () => {
 const ref = useRef(null);
     useEffect(() => {
-        mount(ref.current)
+        mount(ref.current,{
+            onNavigate:()=>{
+                console.log("Container notice navigation in Marketing App.")
+            }
+        })
     }, []);
 return <div ref={ref}>
 
--- mfp/packages/marketing/src/bootstrap.tsx
+++ mfp/packages/marketing/src/bootstrap.tsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import {createMemoryHistory} from "history"
 import App from "./App";
 
-const mount=(el: ReactDOM.Container | null)=>{
-    const history = createMemoryHistory();
+interface INavigation {
+    onNavigate:()=>void;
+}
 
-    ReactDOM.render(<App history={history}/>,el);
+
+const mount = (el: ReactDOM.Container | null, navigation?: INavigation) => {
+    const history = createMemoryHistory();
+    if (navigation?.onNavigate) {
+        history.listen(navigation.onNavigate);
+    }
+    ReactDOM.render(<App history={history} />, el);
 };
 if(process.env.NODE_ENV === "development"){
     const devRoot = document.getElementById("_marketing-dev-root");
@@ -14,4 +21,4 @@ if(process.env.NODE_ENV === "development"){
         mount(devRoot);
     }
 }
-export { mount}
+export { mount};
